57aa587f82a2ead75065ecc9b026d17809fdee38
coser <615684386@qq.com>
2022-07-05 22:00:49 +0800

    Day-04

--- gateway/src/main/java/com/java/gateway/config/SecurityWebfluxConfig.java
+++ gateway/src/main/java/com/java/gateway/config/SecurityWebfluxConfig.java
@@ -1,12 +1,8 @@
 package com.java.gateway.config;
 
-import com.java.gateway.filter.JwtAuthorizationManager;
 import com.java.gateway.filter.TokenAuthenticationConverter;
-import com.java.gateway.provider.TokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -23,20 +19,28 @@ import org.springframework.web.util.pattern.PathPatternParser;
 @EnableWebFluxSecurity
 public class SecurityWebfluxConfig {
 
-    @Autowired
-    private TokenProvider tokenProvider;
-
     //security的鉴权排除的url列表
     public static final String[] excludedAuthPages = {"/health"};
 
-    //只需要登录就可以操作的url
-    public static final String[] authenticatedPages = {"/au"};
-
     @Bean
-    SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+    SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) {
         http
                 // 禁用 CSRF
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                // 配置 CORS 策略
+                .cors(corsSpec -> corsSpec.configurationSource(exchange -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.addAllowedOrigin("*");
+                    // 允许提交的请求头，*表示全部允许
+                    config.addAllowedHeader("*");
+                    // 允许提交的请求方法，*表示全部允许
+                    config.addAllowedMethod("*");
+                    // 允许cookies跨域
+                    config.setAllowCredentials(true);
+                    // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+                    config.setMaxAge(18000L);
+                    return config;
+                }))
                 /*
                   WebFlux 应用程序中的安全上下文存储在 ServerSecurityContextRepository 中。
                   它的 WebSessionServerSecurityContextRepository 实现（默认使用）将上下文存储在会话中。
@@ -45,22 +49,12 @@ public class SecurityWebfluxConfig {
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .logout(ServerHttpSecurity.LogoutSpec::disable);
-        http
+                .logout(ServerHttpSecurity.LogoutSpec::disable)
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.matchers(EndpointRequest.to("health", "info")).permitAll())
-                // OPTIONS 请求方法全部通行
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(HttpMethod.OPTIONS).permitAll())
-                // TRACE 请求方法全部拦截
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(HttpMethod.TRACE).denyAll())
                 // excludedAuthPages 中的路径全部通行
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(excludedAuthPages).permitAll())
-                // authenticatedPages 中的路径全部登录验证权限
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(authenticatedPages).authenticated())
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers("/**").access(new JwtAuthorizationManager(tokenProvider)))
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated());
-        http
-                // 配置 corsFilter 策略
-                .addFilterAt(corsFilter(), SecurityWebFiltersOrder.CORS)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                // 配置权限验证Filter
                 .addFilterAt(webFilter(), SecurityWebFiltersOrder.AUTHORIZATION);
         return http.build();
     }
